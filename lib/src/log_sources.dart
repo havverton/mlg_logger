f9ba380d8feb4ba5258bce0603e09678b574985e
Zubelevich_gm <zubelevich_gm@magnit.ru>
2023-01-16 13:40:43 +0300

    some refs

--- lib/src/log_sources.dart
+++ lib/src/log_sources.dart
@@ -2,10 +2,3 @@ abstract class LogSource{
   void handleLog(List<String> lines);
 }
 
-class ConsoleLogSource implements LogSource{
-  @override
-  void handleLog(List<String> lines) {
-    for (var e in lines) { print(e); }
-  }
-
-}
